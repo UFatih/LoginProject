fa807f86555e161576061488c009bac301b9e214
UFatih <fatihkusaslan@yahoo.com>
2025-07-16 17:20:08 +0300

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Business;
+using Business.Interface;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginProject.Controllers
+{
+    public class LoginController : Controller 
+    {
+        private readonly IUserService _userService;
+
+        public LoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpGet]
+        public IActionResult Loginn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Loginn(BaseUser entity)
+        {
+
+            if (_userService.ValidateUser(entity.email, entity.password))
+            {
+                return View("Success");
+            }
+            else
+            {
+                return View("Error");
+            }
+
+        }
+
+        public IActionResult Profile() 
+        { 
+            return View();
+        }
+    }
+}
